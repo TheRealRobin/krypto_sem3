442d33380c3338daa72de40f308fbbb9552e1990
TheRealRobin <x342lp@gmail.com>
2022-01-07 15:28:23 +0100

    implemented: minides multithreading

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+# coding=utf-8
 import mini_des
 
-#setup data
-plaintext = "Hallo Welt, díês ìst ein Test-PlainText mit seltsamen Zeichen ß♠☼Ö保育園ほいくえんにらえた。😀😀"
-key = mini_des.generate_key("KryptografischerSchlüssel")
+if __name__ == "__main__":
 
-#run encryption
-enc = mini_des.encrypt_text(plaintext, key)
-print(enc)
+    n = 8 # number of threads
 
-#run decryption
-dec = mini_des.decrypt_text(enc, key)
-if (plaintext == dec):
-    print("decryption successful.")
-else: print("decryption FAILED.")
+    #setup data
+    plaintext = "Dies ist eine wirklich wichtige und geheime Nachricht, die nichtmal die NSA sehen darf! (Auch wenn die NSA sicherlich einfach an die Lösung kommt....) Denn der crazy stuff ist wirklich sehr stark... saduiztgbfgbfvvawiuzfthiav7oww  zf87waezf i87qw6e6eatzt zf bfb98qwezf iuwrazzgfwisdaf b86awegf kwzduagfwiuarzbf uizsadbf987q234hf iwqh foiqjfoiuewrhoghke0gi3298zti7erzt234q9583745765873462873658734658732465782346593457569238dsfjbngvsdkjvnbsdkfjhgsdklfjgn                         uwzerfur otiwero9 8tzu2n0983u590t09h"
+    key = mini_des.generate_key("hier-könnte-ihr-passwort-stehen!denn-es-ist-recht-lang-lulundlol27364823")
+
+    #run encryption
+    enc = mini_des.encrypt_text(plaintext, key, n)
+    print("======ENCRYPTED MESSAGE======")
+    print(enc)
+    print(len(enc))
+
+    #run decryption
+    dec = mini_des.decrypt_text(enc, key, n)
+    print("======DECRYPTED MESSAGE======")
+    print(dec)
+    print("\n\n")
+    if (plaintext == dec):
+        print("decryption successful.")
+    else: print("decryption FAILED.")
--- mini_des.py
+++ mini_des.py
@@ -1,6 +1,8 @@
-from bitstring import BitArray, pack
+from bitstring import BitArray, pack as __pack
+from multiprocessing import Pool as __Pool
+from numpy.lib.shape_base import array_split as __array_split
 
-def slice_plaintext(plaintxt: str) -> list(BitArray()): # splits given string into list of BitArrays each of length 8.
+def __slice_plaintext(plaintxt: str) -> list(BitArray()): # splits given string into list of BitArrays each of length 8.
     bytearr = plaintxt.encode("utf-8","strict")
     plainbits = BitArray(bytes=bytearr) #convert str to BitArray
     slices = []
@@ -13,7 +15,7 @@ def slice_plaintext(plaintxt: str) -> list(BitArray()): # splits given string in
             raise RuntimeError("Slice should be 8 bits long, is " + len(slice) + " bits long instead. Please investigate!")
     return slices
 
-def unslice_plaintext(plainslices: list) -> str: # reverses slice_plaintext: uses list of 8-bit long BitArrays to reconstruct unicode strings
+def __unslice_plaintext(plainslices: list) -> str: # reverses slice_plaintext: uses list of 8-bit long BitArrays to reconstruct unicode strings
     concat = BitArray()
     for slice in plainslices:
         concat.append(slice)
@@ -21,7 +23,7 @@ def unslice_plaintext(plainslices: list) -> str: # reverses slice_plaintext: use
     plaintxt = encoded_bytes.decode("utf-8","ignore")
     return plaintxt
 
-def slice_cyphertext(cyphertxt: str) -> list(BitArray()):
+def __slice_cyphertext(cyphertxt: str) -> list(BitArray()):
     slices = []
     if len(cyphertxt) % 2 == 0:
         times = len(cyphertxt) // 2
@@ -31,14 +33,14 @@ def slice_cyphertext(cyphertxt: str) -> list(BitArray()):
         return slices
     else: raise ValueError("cyphertext has to have a length divisible by 2!")
 
-def unslice_cyphertext(cypherslices: list) -> str: # reverses slice_cyphertext: uses list of 8-bit long BitArrays to reconstruct unicode strings
+def __unslice_cyphertext(cypherslices: list) -> str: # reverses slice_cyphertext: uses list of 8-bit long BitArrays to reconstruct unicode strings
     concat = BitArray()
     for slice in cypherslices:
         concat.append(slice)
     encoded_str = str(concat.hex)
     return encoded_str
 
-def expand_input(inp: BitArray) -> BitArray(length=6):
+def __expand_input(inp: BitArray) -> BitArray(length=6):
     # b1 b2 b3 b4 -> b1 b4 b3 b2 b4 b1
     inp_4 = BitArray(length=4)
     if len(inp) <= 4:
@@ -48,11 +50,11 @@ def expand_input(inp: BitArray) -> BitArray(length=6):
             inp_4.overwrite(inp, amount_of_leading_zeroes)
         elif len(inp) == 4:
             inp_4 = inp
-        result = pack("bool=b1, bool=b4, bool=b3, bool=b2, bool=b4, bool=b1", b1=inp_4[0], b2=inp_4[1], b3=inp_4[2], b4=inp_4[3])
+        result = __pack("bool=b1, bool=b4, bool=b3, bool=b2, bool=b4, bool=b1", b1=inp_4[0], b2=inp_4[1], b3=inp_4[2], b4=inp_4[3])
         return result
     else: raise ValueError("Length of Input BitArray has to be between 0 and 4")
 
-def xor(a: BitArray, b: BitArray) -> BitArray:
+def __xor(a: BitArray, b: BitArray) -> BitArray:
     if len(a) == len(b):
         xor = BitArray()
         i = 0
@@ -69,7 +71,7 @@ def xor(a: BitArray, b: BitArray) -> BitArray:
         return xor
     else: raise ValueError("a and b need to be of the same length!")
 
-def s_box(xorin: BitArray) -> BitArray:
+def __s_box(xorin: BitArray) -> BitArray:
     s1 = [[0,3,1,2],
           [2,3,0,1]] # c123 = 0 00
     s2 = [[2,1,3,0],
@@ -105,98 +107,135 @@ def s_box(xorin: BitArray) -> BitArray:
             d1234.overwrite(d34[0:2],2)
     return d1234
 
-def mini_des_step(round_input: BitArray(length=4), round_key: BitArray(length=6)) -> BitArray(length=4):
+def __mini_des_step(round_input: BitArray(length=4), round_key: BitArray(length=6)) -> BitArray(length=4):
     # expand input from 4 to 6 bits
-    expin = expand_input(round_input)
+    expin = __expand_input(round_input)
     # XOR with key
-    xorin = xor(expin, round_key)
+    xorin = __xor(expin, round_key)
     # Feed into S-Boxes (6bit -> 4bit)
-    sboxd = s_box(xorin)
+    sboxd = __s_box(xorin)
     # return the cyphertext for this round
     return sboxd
 
-# runs all n rounds of mini-des for one textblock(8 bit) with key (n *6 bit)
-# 8bit TEXT; 
-def do_encrypt_block_rounds(cleartext_block: BitArray(), sym_block_key: BitArray()) -> BitArray:
-    # rounds = 2 # like in Krypt12.mp4
-    # IDEA: Make rounds depend on key length, so with increased length more rounds are done
-    # keylength can be: n * 6 [with n>1], so 12 18 etc
-    if (len(sym_block_key) % 6 == 0):
-        rounds = len(sym_block_key) // 6
-        rnd_keys = []
+def __encrypt_blocks_thread(cleartext_blocks: list(BitArray()), sym_block_key: BitArray()) -> BitArray: # runs all n rounds of mini-des for one textblock(8 bit) with key (n *6 bit)
+    cyphertext_result = BitArray()
+    todo = len(cleartext_blocks)
+    done = 0
+    for cleartext_block in cleartext_blocks:
+        if (done == 0): print(".",end="",flush=True)
+        elif (done == 1*(todo//4)): print("-",end="",flush=True)
+        elif (done == 2*(todo//4)): print("~",end="",flush=True)
+        elif (done == 3*(todo//4)): print("+",end="",flush=True)
+        elif (done+1 == todo): print("#",end="",flush=True)
+        if (len(sym_block_key) % 6 == 0):
 
-        # sym_block_key would be 12 bit for 2 rounds
-        #                        18 bit for 3
-        # etc.
-        # 
-        for r in range(rounds): # split sym_block_key into smaller round_keys
-            rnd_keys.append(sym_block_key[(r*6):(r*6+6)])
+            # TODO: rounds = 2 # like in Krypt12.mp4? Or something else? Also: key-length...
+            # Waiting for Matthes to respond
+            rounds = len(sym_block_key) // 6
+            rnd_keys = []
 
-        #cleartext block has to be 8 bits
-        if len(cleartext_block) == 8:
-            next_left = cleartext_block[0:4]
-            next_right = cleartext_block[4:8] # setup for first round
-            for r in range(rounds): # do rounds
-                # split cleartext_block into left an right half (4bit)
-                left = next_left
-                right = next_right
-                next_left = right # put current right into next left block
-                # next right block is ( left XOR minides(right,key_n) )
-                next_right = xor(left, mini_des_step(right, rnd_keys[r]))
-
-            final_left = next_left
-            final_right = next_right
-
-            cyphertext_block = BitArray()
-            cyphertext_block.append(final_left)
-            cyphertext_block.append(final_right)
-
-            return cyphertext_block
-        else: raise ValueError("cleartxt_block needs to be of length 8 Bit")
-    else: raise ValueError("block_key needs to be of length 6n with n as amount of rounds (currently 2)")
-
-# runs all n rounds of mini-des for one textblock(8 bit) with key (n *6 bit)
-# 8bit TEXT;FIXME:
-def do_decrypt_block_rounds(cyphertext_block: BitArray(), sym_block_key: BitArray()) -> BitArray:
-    # rounds = 2 # like in Krypt12.mp4
-    if (len(sym_block_key) % 6 == 0):
-        rounds = len(sym_block_key) // 6 # rounds depends on keylen
-        rnd_keys = []
-
-        # sym_block_key would be 12 bit for 2 rounds
-        #                        18 bit for 3
-        # etc.
-        # Problem: Key (not block_key) cannot be infinite, FIXME: look for better way of getting block key
-        for r in range(rounds): # split sym_block_key into smaller round_keys
-            if ( len(sym_block_key) / 6 ) == rounds: # key can be split into exactly n (amount of rounds) round_keys with length 6
+            # sym_block_key would be 12 bit for 2 rounds, 18 bit for 3
+            for r in range(rounds): # split sym_block_key into smaller round_keys
                 rnd_keys.append(sym_block_key[(r*6):(r*6+6)])
-            else: raise ValueError("block_key needs to be of length 6n with n as amount of rounds (currently 2)")
 
-        #cyphertext block has to be 8 bits
-        if len(cyphertext_block) == 8:
-            bot_left = cyphertext_block[0:4]
-            bot_right = cyphertext_block[4:8]
-            for r in range(rounds-1, -1, -1): # example: rounds=3-> r : 2,1,0
-                top_right = bot_left
-                top_left = xor(bot_right,mini_des_step(top_right, rnd_keys[r]))
-                bot_left = top_left
-                bot_right = top_right
-            
-            cleartext_block = BitArray()
-            cleartext_block.append(bot_left)
-            cleartext_block.append(bot_right)
-            return cleartext_block
-        else: raise ValueError("cyphertext_block needs to be exactly 8 bits.")
-    
-    else: raise ValueError("block_key needs to be of length 6n with n as amount of rounds (currently 2)")
+            #cleartext block has to be 8 bits
+            if len(cleartext_block) == 8:
+                next_left = cleartext_block[0:4]
+                next_right = cleartext_block[4:8] # setup for first round
+                for r in range(rounds): # do rounds
+                    # split cleartext_block into left an right half (4bit)
+                    left = next_left
+                    right = next_right
+                    next_left = right # put current right into next left block
+                    # next right block is ( left __xor minides(right,key_n) )
+                    next_right = __xor(left, __mini_des_step(right, rnd_keys[r]))
 
-def encrypt_text(plaintxt: str, symmetric_key: BitArray()) -> str:
-    plain_blocks = slice_plaintext(plaintxt)
+                final_left = next_left
+                final_right = next_right
+                
+                cyphertext_result.append(final_left)
+                cyphertext_result.append(final_right)
+            else: raise ValueError("cleartxt_block needs to be of length 8 Bit")
+        else: raise ValueError("block_key needs to be of length 6n with n as amount of rounds (currently 2)")
+        done += 1
+    return cyphertext_result
+
+def __decrypt_blocks_thread(cyphertext_blocks: list(BitArray()), sym_block_key: BitArray()) -> BitArray: # runs all n rounds of mini-des for one textblock(8 bit) with key (n *6 bit)
+    cleartext_result = BitArray()
+    todo = len(cyphertext_blocks)
+    done = 0
+    for cyphertext_block in cyphertext_blocks:
+        if (done == 0): print(".",end="",flush=True)
+        elif (done == 1*(todo//4)): print("-",end="",flush=True)
+        elif (done == 2*(todo//4)): print("~",end="",flush=True)
+        elif (done == 3*(todo//4)): print("+",end="",flush=True)
+        elif (done+1 == todo): print("#",end="",flush=True)
+        if (len(sym_block_key) % 6 == 0):
+
+            #TODO: rounds = 2 # like in Krypt12.mp4? waiting for Matthes answer to email
+            rounds = len(sym_block_key) // 6 # rounds depends on keylen 
+            rnd_keys = []
+
+            # sym_block_key would be 12 bit for 2 rounds 18 bit for 3
+            for r in range(rounds): # split sym_block_key into smaller round_keys
+                if ( len(sym_block_key) / 6 ) == rounds: # key can be split into exactly n (amount of rounds) round_keys with length 6
+                    rnd_keys.append(sym_block_key[(r*6):(r*6+6)])
+                else: raise ValueError("block_key needs to be of length 6n with n as amount of rounds (currently 2)")
+
+            #cyphertext block has to be 8 bits
+            if len(cyphertext_block) == 8:
+                bot_left = cyphertext_block[0:4]
+                bot_right = cyphertext_block[4:8]
+                for r in range(rounds-1, -1, -1): # example: rounds=3-> r : 2,1,0
+                    top_right = bot_left
+                    top_left = __xor(bot_right,__mini_des_step(top_right, rnd_keys[r]))
+                    bot_left = top_left
+                    bot_right = top_right
+                
+                final_left = bot_left
+                final_right = bot_right
+
+                cleartext_result.append(bot_left)
+                cleartext_result.append(bot_right)     
+            else: raise ValueError("cyphertext_block needs to be exactly 8 bits.")
+        else: raise ValueError("block_key needs to be of length 6n with n as amount of rounds (currently 2)")
+        done += 1
+    return cleartext_result
+
+def __run_encryption_threads(plain_blocks: list(BitArray()), symmetric_key: BitArray(), n: int) -> BitArray: # spawns n new processes that encrypts all the blocks and returns the encrypted Block-List
+    print("Using "+str(n)+" threads. Be patient!")
     crypt_blocks = []
+    thread_sized_plain_chunks = __array_split(plain_blocks, n)
+    p = __Pool(n)
+    args = []
+    for i in range(n):
+        args.append([thread_sized_plain_chunks[i], symmetric_key])
+    #multithread encryption
+    print("Encrypting",end="",flush=True)
+    crypt_blocks = p.starmap(__encrypt_blocks_thread, tuple(args))
+    print("")
+    return crypt_blocks
+
+def __run_decryption_threads(cypher_blocks: list(BitArray()), symmetric_key: BitArray(), n: int) -> BitArray(): # spawns n new processes that decrypts all the blocks and returns the encrypted Block-List
+    print("Using "+str(n)+" threads. Be patient!")
+    decrypt_blocks = []
+    thread_sized_encrypted_chunks = __array_split(cypher_blocks, n)
+    p = __Pool(n)
+    args = []
+    for i in range(n):
+        args.append([thread_sized_encrypted_chunks[i], symmetric_key])
+    #multithread decryption
+    print("Decrypting",end="",flush=True)
+    decrypt_blocks = p.starmap(__decrypt_blocks_thread, tuple(args))
+    print("")
+    return decrypt_blocks
+
+def encrypt_text(plaintxt: str, symmetric_key: BitArray(), threads: int) -> str:
+    plain_blocks = __slice_plaintext(plaintxt)
     # Number of Rounds depends on key length. Only keys with n*6 Bit are allowed, else fill up with zeroes
     repeat = True
     key_isgood = False
-    while repeat:
+    while repeat: # Test if key is okay or needs to be filled to specific size with zeroes
         if (len(symmetric_key) % 6 == 0): # key len is multiple of 6, so n*6 requirement is met
             repeat = False
             key_isgood = True
@@ -205,18 +244,13 @@ def encrypt_text(plaintxt: str, symmetric_key: BitArray()) -> str:
             zeroes = BitArray(diff * '0b0')
             symmetric_key.append(zeroes)
     if key_isgood:
-        done = 1
-        todo = len(plain_blocks)
-        for block in plain_blocks:
-            print("Encrypting " + str(done) + "/" +str(todo) + "...", end="\r")
-            crypt_blocks.append(do_encrypt_block_rounds(block, symmetric_key))
-            done += 1
+        crypt_blocks = __run_encryption_threads(plain_blocks, symmetric_key, threads)
+    else: raise ValueError("Key is not good! Investigate") #TODO: besser formulieren
     print("Finished encrypting.")
-    return unslice_cyphertext(crypt_blocks)
+    return __unslice_cyphertext(crypt_blocks)
 
-def decrypt_text(cyphertxt: str, symmetric_key: BitArray()) -> str:
-    cypher_blocks = slice_cyphertext(cyphertxt)
-    decrypt_blocks = []
+def decrypt_text(cyphertxt: str, symmetric_key: BitArray(), threads: int) -> str:
+    cypher_blocks = __slice_cyphertext(cyphertxt)
     # Number of rounds depends on key length. Only keys with n*6 Bit are allowed, else fill up with zeroes
     repeat = True
     key_isgood = False
@@ -229,14 +263,10 @@ def decrypt_text(cyphertxt: str, symmetric_key: BitArray()) -> str:
             zeroes = BitArray(diff * '0b0')
             symmetric_key.append(zeroes)
     if key_isgood:
-        done = 1
-        todo = len(cypher_blocks)
-        for block in cypher_blocks:
-            print("Decrypting " + str(done) + "/" + str(todo) + "...", end="\r")
-            decrypt_blocks.append(do_decrypt_block_rounds(block, symmetric_key))
-            done += 1
-        print("Finished decrypting.")
-    return unslice_plaintext(decrypt_blocks)
+        decrypt_blocks = __run_decryption_threads(cypher_blocks, symmetric_key, threads)
+    else: raise ValueError("Key is not good! Investigate") #TODO: besser formulieren
+    print("Finished decrypting.")
+    return __unslice_plaintext(decrypt_blocks)
 
 def generate_key(passphrase: str) -> BitArray():
     p_bytes = passphrase.encode("utf-8","strict")
